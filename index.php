2559d85c43f96cb307595fd71aec87e76b8f78bc
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-01-23 18:43:17 +0600

    index page added

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Welcome to Dig-Out</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+
+<body>
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="img/logo.png">
+        </div>
+
+        <div class="searchContainer">
+            <input type="text" name="term" class="searchBox">
+            <input type="submit" value="Search" class="searchButton">
+        </div>
+    </div>
+</body>
+</html>
