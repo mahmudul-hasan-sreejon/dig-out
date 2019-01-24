b44d4afc51320199f9c5d2accce4179e5b42088a
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-01-24 13:32:04 +0600

    search page initialized

---
+++ search.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Welcome to Dig-Out</title>
+
+    <link rel="shortcut icon" type="image/ico" href="assets/img/favicon.ico" />
+    
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css" />
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <div class="header">
+            <div class="headerContent">
+                <div class="logoContainer">
+                    <a href="index.php">
+                        <img src="assets/img/logo.png">
+                    </a>
+                </div>
+
+                <div class="searchContainer">
+                    <form action="search.php" method="GET">
+                        <div class="searchBarContainer">
+                            <input type="text" class="searchBox" name="term">
+                            <button>Search</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+</html>
