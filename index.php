faceb78666a1edfed6c1d0da96c66fd1056454a1
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-01-24 13:12:58 +0600

    favicon added

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Welcome to Dig-Out</title>
+
+    <link rel="shortcut icon" type="image/ico" href="assets/img/favicon.ico" />
     
     <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css" />
 </head>
