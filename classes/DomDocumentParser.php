d9938d3e82501a9c1eb37bb72db798b1b4a0943f
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-01-25 14:32:56 +0600

    constructor for DomDocumentParser class added

--- classes/DomDocumentParser.php
+++ classes/DomDocumentParser.php
@@ -2,9 +2,11 @@
 
 class DomDocumentParser {
     public function __construct($url) {
-        $options = array(
-            "https"=>array("method"=>"GET", "header"=>"User-Agent: digOutBot/0.1\n"),
-        );
+        $options = array("https"=>array("method"=>"GET", "header"=>"User-Agent: digOutBot/0.1\n"));
+        $context = stream_context_create($options);
+
+        $doc = new DOMDocument();
+        @$doc->loadHTML(file_get_contents($url, false, $context));
     }
 }
 
