28053d12943d568974bbb5bee72475735d11a6e2
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-01-25 16:27:39 +0600

    converting relative links to absolute links

--- crawl.php
+++ crawl.php
@@ -2,6 +2,19 @@
 
 include("classes/DomDocumentParser.php");
 
+function createLink($src, $url) {
+    $scheme = parse_url($url)["scheme"];
+    $host = parse_url($url)["host"];
+
+    if(substr($src, 0, 2) == "//") $src = $scheme . ":" . $src;
+    else if(substr($src, 0, 1) == "/") $src = $scheme . "://". $host . $src;
+    else if(substr($src, 0, 2) == "./") $src = $scheme . "://". $host . dirname(parse_url($url)["path"]) . substr($src, 1);
+    else if(substr($src, 0, 3) == "../") $src = $scheme . "://". $host . "/" . $src;
+    else if(substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") $src = $scheme . "://". $host . "/" . $src;
+
+    return $src;
+}
+
 function followLinks($url) {
     $parser = new DomDocumentParser($url);
 
@@ -20,11 +33,13 @@ function followLinks($url) {
             continue;
         }
 
+        $href = createLink($href, $url);
+
         echo $href."<br>";
     }
 }
 
-$url = "https://mahmudul-hasan-sreejon.github.io/";
+$url = "https://www.bbc.com";
 followLinks($url);
 
 ?>
