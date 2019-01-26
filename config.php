908f5737bb5e5c24473fffc9125f2d756bd51c3e
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-01-27 01:13:51 +0600

    database config file added

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+ob_start();
+
+try {
+    $conn = new PDO("mysql: dbname = dig_out; host = localhost", "root", "1");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+catch(PDOException $e) {
+    echo "Connection failed: ". $e->getMessage();
+}
+
+?>
