e0bb0425f0f4ed447cab2637659f8c427520eeae
mahmudul-hasan-sreejon <mahmudul.hasan.sreejon@gmail.com>
2021-03-27 23:48:56 +0600

    Add hooks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,93 @@
 let timer;
 
 $(document).ready(function() {
-    $(".result").on("click auxclick", function(e) {
-        let id = $(this).attr("data-linkId");
-        let url = $(this).attr("href");
+	$('.result').on('click auxclick', function(e) {
+		let id = $(this).attr('data-linkId');
+		let url = $(this).attr('href');
 
-        // if(!id) alert("data-linkId attribute not found...");
+		// if(!id) alert("data-linkId attribute not found...");
 
-        increaseLinkClicks(id, url, e.type);
+		increaseLinkClicks(id, url, e.type);
 
-        return false;
-    });
+		return false;
+	});
 
-    let grid = $(".imageResults");
+	let grid = $('.imageResults');
 
-    grid.on("layoutComplete", function() {
-        $(".gridItem img").css("visibility", "visible");
-    });
+	grid.on('layoutComplete', function() {
+		$('.gridItem img').css('visibility', 'visible');
+	});
 
-    grid.masonry({
-        itemSelector: ".gridItem",
-        columnWidth: 200,
-        gutter: 5,
-        isInitLayout: false
-    });
+	grid.masonry({
+		itemSelector: '.gridItem',
+		columnWidth: 200,
+		gutter: 5,
+		isInitLayout: false
+	});
 
-    $("[data-fancybox]").fancybox({
-        caption : function(instance, item) {
-            let caption = $(this).data('caption') || '';
-            let siteUrl = $(this).data('siteurl') || '';
-    
-            if(item.type === 'image') {
-                caption = (caption.length ? caption + '<br />' : '') + '<a href="' + item.src + '">View image</a>' + '<br />' + '<a href="' + siteUrl + '">View page</a>';
-            }
-    
-            return caption;
-        },
+	$('[data-fancybox]').fancybox({
+		caption: function(instance, item) {
+			let caption = $(this).data('caption') || '';
+			let siteUrl = $(this).data('siteurl') || '';
 
-        afterShow : function(instance, item) {
-            increaseImageClicks(item.src);
-        }
-    });
+			if (item.type === 'image') {
+				caption =
+					(caption.length ? caption + '<br />' : '') +
+					'<a href="' +
+					item.src +
+					'">View image</a>' +
+					'<br />' +
+					'<a href="' +
+					siteUrl +
+					'">View page</a>';
+			}
+
+			return caption;
+		},
+
+		afterShow: function(instance, item) {
+			increaseImageClicks(item.src);
+		}
+	});
 });
 
 function loadImage(src, className) {
-    var image = $("<img>");
+	var image = $('<img>');
 
-    image.on("load", function() {
-        $("." + className + " a").append(image);
+	image.on('load', function() {
+		$('.' + className + ' a').append(image);
 
-        clearTimeout(timer);
-        timer = setTimeout(function() {
-            $(".imageResults").masonry();
-        }, 500);
-    });
+		clearTimeout(timer);
+		timer = setTimeout(function() {
+			$('.imageResults').masonry();
+		}, 500);
+	});
 
-    image.on("error", function() {
-        $("." + className).remove();
+	image.on('error', function() {
+		$('.' + className).remove();
 
-        $.post("ajax/setBroken.php", {src: src});
-    });
+		$.post('hooks/setBroken.php', { src: src });
+	});
 
-    image.attr("src", src);
+	image.attr('src', src);
 }
 
 function increaseLinkClicks(linkId, url, type) {
-    $.post("ajax/updateLinkCount.php", {linkId: linkId})
-    .done(function(result) {
-        if(result != "") {
-            alert(result);
-            return;
-        }
+	$.post('hooks/updateLinkCount.php', { linkId: linkId }).done(function(result) {
+		if (result != '') {
+			alert(result);
+			return;
+		}
 
-        if(type == "click") window.location.href = url;
-    });
+		if (type == 'click') window.location.href = url;
+	});
 }
 
 function increaseImageClicks(imageUrl) {
-    $.post("ajax/updateImageCount.php", {imageUrl: imageUrl})
-    .done(function(result) {
-        if(result != "") {
-            alert(result);
-            return;
-        }
-    });
+	$.post('hooks/updateImageCount.php', { imageUrl: imageUrl }).done(function(result) {
+		if (result != '') {
+			alert(result);
+			return;
+		}
+	});
 }
